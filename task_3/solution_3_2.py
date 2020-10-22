882651ac0cbeac0dacaeba2f27538eeb3c4e81ae
lilovyjgrib <71791965+lilovyjgrib@users.noreply.github.com>
2020-10-23 01:24:21 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -1,9 +1,9 @@
 alphabet = ''
 all_input = ''
 for a in 'a'*10:
-  user_input = input()
-  all_input = all_input + user_input
+    user_input = input()
+        all_input = all_input + user_input
 for letter in all_input:
-  if letter not in alphabet:
-    alphabet = alphabet + letter
+    if letter not in alphabet:
+        alphabet = alphabet + letter
 print(alphabet)
