ce99a8658fbfacfa89f2888fd6412b128883ec60
lilovyjgrib <71791965+lilovyjgrib@users.noreply.github.com>
2020-10-23 01:24:55 +0300

    Update solution_3_1.py

--- task_3/solution_3_1.py
+++ task_3/solution_3_1.py
@@ -1,6 +1,6 @@
 alphabet = ''
 user_input = input()
 for letter in user_input:
-  if letter not in alphabet:
-    alphabet = alphabet + letter
+    if letter not in alphabet:
+        alphabet = alphabet + letter
 print(alphabet)
