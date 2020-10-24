1b64a7c1623cc29ffe2a8139c8cd36ad1544f4dd
lilovyjgrib <71791965+lilovyjgrib@users.noreply.github.com>
2020-10-25 02:03:27 +0300

    Update solution_12.py

--- task_12/solution_12.py
+++ task_12/solution_12.py
@@ -1,1 +1,22 @@
-# your code here
+# решение пока не работает на примерах с нечётным количеством одинаковых букв подряд
+word = input()
+occured_twice = False
+
+d = ''
+for t in word:
+    bigram = d + t
+    bigram_counter = 0
+    n = ''
+    if d != '':
+        for m in word:
+            bigram2 = n + m
+            if n != '':
+                if bigram2 == bigram:
+                    bigram_counter += 1
+                if bigram_counter == 2:
+                    occured_twice = True
+
+            n = m
+    d = t
+    print('\n')
+print(occured_twice)
