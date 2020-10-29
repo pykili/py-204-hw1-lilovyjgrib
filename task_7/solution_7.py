396f590a8e3e535d8c9622f44d6eff3fd5b1c917
lilovyjgrib <71791965+lilovyjgrib@users.noreply.github.com>
2020-10-29 23:44:00 +0300

    исправил

--- task_7/solution_7.py
+++ task_7/solution_7.py
@@ -1,9 +1,18 @@
-#я уже понял что решение не соответствует условию((
 word = input()
 rev_word = ''
-l = len(word)
-while l > 0:
-    rev_word = rev_word + word[l - 1]
-    l = l - 1
+len_count = 0
+i_index = 0
+a_index = 0
+
+for letter in word:
+    len_count += 1
+for i in word:
+    for a in word:
+        if a_index == len_count - i_index - 1:
+            rev_word += a
+        a_index += 1
+    a_index = 0
+    i_index += 1
+    
 is_palindrom = rev_word == word
 print(is_palindrom)
